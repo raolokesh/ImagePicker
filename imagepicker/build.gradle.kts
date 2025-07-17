41edf2a73ade8c8084e465aa116bfa248b460127
Lokesh <LRao@VaaaNinfra.Org>
2025-07-17 10:36:14 +0530

    Change in Ui and Permission

--- imagepicker/build.gradle.kts
+++ imagepicker/build.gradle.kts
@@ -75,7 +75,7 @@ publishing {
         create("release", MavenPublication::class) {
             groupId = "com.github.raolokesh"
             artifactId = "imagepicker"
-            version = "1.0.11"
+            version = "1.0.12"
 
             afterEvaluate {
                 from(components["release"])
